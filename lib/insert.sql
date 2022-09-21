df9e9166290fe477bf2c184863d0e6d1497beb2a
Duy <ericpham19@outlook.com>
2022-09-21 13:55:08 +1000

    made bears

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears ( name , age, sex, color,temperament, alive)
+VALUES ("Mr. Chocolate" , 10 , "M" , "blue", "calm" , 1),
+    ("Rowdy" , 12 , "M" , "blue", "calm" , 1),
+    ("Tabitha" , 10 , "F" , "blue", "calm" , 1),
+    ("Sergeant Brown" , 10 , "M" , "blue", "calm" , 0),
+    ("Melissa" , 10 , "F" , "red", "goofy", 1),
+    ("Grinch" , 10 , "M" , "green", "calm" , 1),
+    ("Wendy" , 10 , "F" , "blue", "goofy" , 0),
+    (null , 10 , "F" , "blue", "calm" , 0);
+
+
